105c1561bfad652e916c7963a4b88da299a5b837
Johnkkzx <jpreal1540@gmail.com>
2026-06-23 20:57:15 -0300

    Delete bot registro directory

--- bot registro/src/events/guildMemberAdd.js
+++
@@ -1,8 +0,0 @@
-module.exports = (client) => {
-  client.on('guildMemberAdd', async member => {
-    const cargoId = '1516550378861105162';
-
-    await member.roles.add(cargoId).catch(() => {});
-    await member.setNickname(`Cidadão. ${member.user.username}`).catch(() => {});
-  });
-};
